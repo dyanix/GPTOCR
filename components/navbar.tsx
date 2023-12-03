a0d458d01a3a2b2f1b32c9a3cb0b79ea1617162d
dyanix <dyanixdhawale@gmail.com>
2023-12-03 14:17:41 +0530

    c

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ const NavBar = async() => {
     const apiLimitCount = await getApiLimitCount();
     return (
         <div className="flex items-center p-4">
-            <MobileSidebar apiLimitCount={apiLimitCount}  />
+            <MobileSidebar apiLimitCount={apiLimitCount} isPro={false}  />
             <div className="flex w-full  justify-end space-x-7 ">
                 <div className="mt-1">
                     <UserButton afterSignOutUrl="/" />
